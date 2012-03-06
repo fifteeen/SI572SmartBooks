0ea1b2444da9e170ec255c084b417fe62d32ed9b
kenwyq <kenwyq@gmail.com>
2012-03-06 13:47:36 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -28,3 +28,11 @@ VALUES ("firstemail@gmail.com", "123456", "Adam"),
 -- below from Daodao: create Book Table
 CREATE TABLE Book(id INT UNSIGNED NOT NULL AUTO_INCREMENT KEY, name VARCHAR(128), ISBN INTEGER, year INTEGER, edition INTEGER, price FLOAT, quantity INTEGER, description TEXT, author VARCHAR(128), course_id INTEGER);
 
+-- below from Yanqi: create table course
+create table course(id INT UNSIGNED NOT NULL AUTO_INCREMENT KEY, name varchar(128), description text(1024), num integer, term varchar(128));
+
+-- create table costomer
+create table customer(id int unsigned not null auto_increment key,email varchar(128),password varchar(128));
+
+--create table shoppingcart
+create table shoppingcart(id int unsigned not null auto_increment key,quantity integer,checkout bool,book_id integer,customer_id integer);
